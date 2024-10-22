878f5cca0e1a80165d9532d842c4a2ea4d803cb0
Krishna Yadav <krishnayadav@MacBook-Pro-3.local>
2024-10-22 18:09:51 +0530

    margin type

---
+++ src/bot/set_margin.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 18:05:16 2024
+
+@author: krishnayadav
+"""
+
+from binance.client import Client
+from binance.enums import SIDE_BUY, SIDE_SELL, ORDER_TYPE_MARKET
+from binance.enums import *
+import json
+import os
+from slack_bot import post_error_to_slack
+
+
+with open('config.json', 'r') as json_file:
+    config = json.load(json_file)
+    
+with open('coin.json', 'r') as json_file:
+    coin_dict = json.load(json_file)
+
+with open('precision.json', 'r') as json_file:
+    precion_dict = json.load(json_file)
+    
+# Initialize the Binance client
+client = Client(config['API_KEY'], config['API_SECRET'], tld='com')
+
+
+#Change margin type to ISOLATED before placing the order
+try:
+    client.futures_change_margin_type(symbol=symbol, marginType="ISOLATED")
+    print(f"Margin type set to ISOLATED for {symbol}.")
+except Exception as e:
+    print(f"Error setting margin type: {e}")
+    post_error_to_slack(f"Error setting margin type for {symbol}: {e}")
