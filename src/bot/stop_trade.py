8ed4086731e3333e53385f318aac4d7cd181154e
Krishna <joi@aigenlayer.org>
2024-10-24 09:24:43 +0530

    ss

--- src/bot/stop_trade.py
+++ src/bot/stop_trade.py
@@ -36,20 +36,41 @@ client = Client(config['API_KEY'], config['API_SECRET'], tld='com')
 #     reduceOnly=True  # Ensure this only reduces the position and doesn't open a new one
 # )
 
-response = client.futures_cancel_order(
-            symbol='AAVEUSDT',
-            orderId = 15039231629
-        )
+# response = client.futures_cancel_order(
+#             symbol='AAVEUSDT',
+#             orderId = 15039231629
+#         )
 
-response = client.futures_cancel_order(
-            symbol='AAVEUSDT',
-            orderId= 15039231609
-        )
+# response = client.futures_cancel_order(
+#             symbol='AAVEUSDT',
+#             orderId= 15039231609
+#         )
 
-close_position_order = client.futures_create_order(
-    symbol='AAVEUSDT',
-    side='SELL',
-    type='MARKET',
-    quantity=0.2,  # Close entire long position
-    reduceOnly=True  # Ensure this only reduces the position and doesn't open a new one
-)
+# close_position_order = client.futures_create_order(
+#     symbol='AAVEUSDT',
+#     side='SELL',
+#     type='MARKET',
+#     quantity=0.2,  # Close entire long position
+#     reduceOnly=True  # Ensure this only reduces the position and doesn't open a new one
+# )
+
+with open(config['data_dir'] + '/proposal_post_live.json', 'r') as json_file:
+    proposal_post_live = json.load(json_file)
+    
+with open('coin.json', 'r') as json_file:
+    coin_symbol = json.load(json_file)
+
+live_coin_symbol = {}
+for key, value in proposal_post_live.items():
+    live_coin_symbol[key] = coin_symbol[proposal_post_live[key]['coin']]
+
+positions = client.futures_position_information()
+
+# Filter for only open positions
+open_coin = []
+open_positions = [pos for pos in positions if float(pos['positionAmt']) != 0]
+for position_dict in open_positions:
+    open_coin.append(position_dict['symbol'])
+    
+
+print(open_positions)
