71b5b35c6e0892573a82f3fcaf1e2c77c5c3c60b
Krishna <joi@aigenlayer.org>
2024-10-24 14:14:18 +0530

    ss

--- src/bot/stop_trade.py
+++ src/bot/stop_trade.py
@@ -14,59 +14,59 @@ from binance.client import Client
 with open('config.json', 'r') as json_file:
     config = json.load(json_file)
 
-# with open('proposal_post_live.json', 'r') as json_file:
-#     live_trade = json.load(json_file)
+with open('proposal_post_live.json', 'r') as json_file:
+    live_trade = json.load(json_file)
 
     
 # Initialize the Binance client
 client = Client(config['API_KEY'], config['API_SECRET'], tld='com')
 
-# response = client.futures_cancel_order(
-#             symbol='CAKEUSDT',
-#             orderId = 1593383729
-#         )
+response = client.futures_cancel_order(
+            symbol='CAKEUSDT',
+            orderId = 1593383729
+        )
 
-# print("Stop loss sucess")
+print("Stop loss sucess")
 
-# response = client.futures_cancel_order(
-#             symbol='CAKEUSDT',
-#             orderId= 1593383740
-#         )
+response = client.futures_cancel_order(
+            symbol='CAKEUSDT',
+            orderId= 1593383740
+        )
 
-# print("Target sucess")
-
-
-# close_position_order = client.futures_create_order(
-#     symbol='CAKEUSDT',
-#     side='SELL',
-#     type='MARKET',
-#     quantity=7686,  # Close entire long position
-#     reduceOnly=True  # Ensure this only reduces the position and doesn't open a new one
-# )
-
-# print("All quantity sucess")
+print("Target sucess")
 
 
-with open(config['data_dir'] + '/proposal_post_live.json', 'r') as json_file:
-    proposal_post_live = json.load(json_file)
+close_position_order = client.futures_create_order(
+    symbol='CAKEUSDT',
+    side='SELL',
+    type='MARKET',
+    quantity=7686,  # Close entire long position
+    reduceOnly=True  # Ensure this only reduces the position and doesn't open a new one
+)
+
+print("All quantity sucess")
+
+
+# with open(config['data_dir'] + '/proposal_post_live.json', 'r') as json_file:
+#     proposal_post_live = json.load(json_file)
     
-with open('coin.json', 'r') as json_file:
-    coin_symbol = json.load(json_file)
+# with open('coin.json', 'r') as json_file:
+#     coin_symbol = json.load(json_file)
 
-live_coin_symbol = {}
-for key, value in proposal_post_live.items():
-    live_coin_symbol[key] = coin_symbol[proposal_post_live[key]['coin']]
+# live_coin_symbol = {}
+# for key, value in proposal_post_live.items():
+#     live_coin_symbol[key] = coin_symbol[proposal_post_live[key]['coin']]
 
-positions = client.futures_position_information()
+# positions = client.futures_position_information()
 
-# Filter for only open positions
-open_coin = []
-open_positions = [pos for pos in positions if float(pos['positionAmt']) != 0]
-for position_dict in open_positions:
-    open_coin.append(position_dict['symbol'])
+# # Filter for only open positions
+# open_coin = []
+# open_positions = [pos for pos in positions if float(pos['positionAmt']) != 0]
+# for position_dict in open_positions:
+#     open_coin.append(position_dict['symbol'])
     
 
-print(open_positions)
+# print(open_positions)
 
 
 
